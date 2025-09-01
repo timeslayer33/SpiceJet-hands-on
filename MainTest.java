aed9ed8ea9a12c7bf8f347f5086748306117920b
timeslayer33 <timeslayer33@gmail.com>
2025-09-01 22:26:08 +0530

    Add files via upload

--- MainTest.java
+++ MainTest.java
@@ -1,4 +1,4 @@
-package spicejetProject;
+package SpiceJet;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,15 +9,20 @@ public class MainTest {
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.spicejet.com/");
+		driver.get("https://www.spicejet.com");
 		driver.manage().window().maximize();
 		Thread.sleep(3000L);
 		driver.findElement(By.xpath("//div[@class='css-1dbjc4n r-14lw9ot r-11u4nky r-z2wwpe r-1phboty r-rs99b7 r-1loqt21 r-13awgt0 r-ymttw5 r-tju18j r-5njf8e r-1otgn73']")).click();
 		driver.findElement(By.xpath("//input[@class='css-1cwyjr8 r-homxoj r-ubezar r-10paoce r-13qz1uu']")).sendKeys("kol");
 		driver.findElement(By.xpath("(//input[@class='css-1cwyjr8 r-homxoj r-ubezar r-10paoce r-13qz1uu'])[2]")).sendKeys("ban");
 		driver.findElement(By.xpath("//div[text()='Kempegowda International Airport']")).click();
-		
+		////div[contains(@class, 'r-y47klf')]
+		//css-1dbjc4n r-1awozwy r-19m6qjp r-156aje7 r-y47klf r-1phboty r-1d6rzhh r-1pi2tsx r-1777fci r-13qz1uu
+		driver.findElement(By.xpath("//div[contains(@class, 'r-y47klf')]")).click();
+		driver.findElement(By.xpath("//div[text()='Passengers']")).click();
+																																																																																						
 
 	}
 
 }
+	
